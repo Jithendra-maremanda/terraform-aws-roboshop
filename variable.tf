9a4a2c6d0ca037c7eb1a9c2dd1a7230271b4dd56
Jithendra-maremanda <jithendramaremanda@gmail.com>
2025-08-17 05:19:51 +0530

    Initial commit after renaming

---
+++ variable.tf
@@ -0,0 +1,32 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+variable "zone_id" {
+    description = "The ID of the Route 53 hosted zone"
+    type        = string
+    default = "Z07875261XFWESB6HKZDD"
+  
+}
+
+variable "zone_name" {
+    type        = string
+    default = "stansrobotshop.site"
+  
+}
+variable "component" {
+  
+}
+
+variable "rule_priority" {
+    description = "Priority of the listener rule"
+    type        = number
+    default     = 10
+  
+}
+variable "rule_header_url" {
+  
+}
